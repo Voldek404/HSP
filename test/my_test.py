3edcc05dc2fe173b6571e2aad4ca38cd291fd117
Vladimir K <iceq3004@yandex.ru>
2024-07-03 16:10:11 +0300

    Update my_test.py

--- test/my_test.py
+++ test/my_test.py
@@ -1,5 +1,6 @@
 import unittest
 from list_sort.my_sort import list_sort
+from list_sort.my_sort import generate_list #импортируем функцию многократной генерации списка
 
 
 class sort_tests(unittest.TestCase):
@@ -21,6 +22,17 @@ class sort_tests(unittest.TestCase):
     def test_ascending(self): #test ascending order
          lst = list_sort([43, 21, 54, 76, 87, 32, 54, 12, 43, 65])
          self.assertTrue(lst[i] <= lst[i + 1] for i in range(len(lst) - 1))
+        
+    def test_empty_list(self): #test with empty list
+        test_list = []
+        lst = list_sort(test_list)
+        self.assertEqual(lst , [])
+
+    def multiple_test(self): #тест с многократной генерацией списка
+        for i in range(5):
+            print(generate_list(10))
+            self.assertEqual(list_sort([generate_list(10)]),
+                             sorted(generate_list(10)))
 
 
 if __name__ == '__main__':
