5d5e2bb7d94f3f5355b6b8e1df5788f5fa95bb46
Vladimir K <iceq3004@yandex.ru>
2024-07-04 12:55:00 +0300

    Update 3.2_next.py

--- file_oper/3.2_next.py
+++ file_oper/3.2_next.py
@@ -1,28 +1,23 @@
 import random
 
 
-# для проверки исключения сделан файл 11.txt
-
-def file_ins(file_list):
+def file_ins(file_list: list[str]) -> int:
     sum_1 = 0
     for names in file_list:
         with open((f'{names}.txt'), 'r') as fi:
             for s in fi:
-                s = s.strip()
-                if not s:  # проверка на пустую строку
-                    pass
                 try:
+                    s = s.strip()
                     sum_1 += int(s)
-                except ValueError:
-                    print(f"Ошибка преобразования строки в число в файле {i}")
-                    return False
-    return sum_1
+                except Exception:
+                    return [0, 1] #сигнализация ошибки
+    return [sum_1,0]
 
 
 a = random.randint(1, 10)
 b = random.randint(1, 10)
-file_list =[a,b]
+file_list = [a,b]
 
 print("Первый номер файла -- ", a)
 print("Второй  номер файла -- ", b)
-print("Сумма шести чисел составляет -- ", file_ins(file_list))
+print("Общее количество золота составляет -- ", file_ins(file_list))
