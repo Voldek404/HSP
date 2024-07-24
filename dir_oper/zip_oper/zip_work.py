1596cad8fe77e7d3e5f640771984e7fc48f23ceb
Vladimir K <iceq3004@yandex.ru>
2024-07-24 09:42:04 +0300

    Update zip_work.py

--- dir_oper/zip_oper/zip_work.py
+++ dir_oper/zip_oper/zip_work.py
@@ -3,17 +3,17 @@ import os
 
 
 def zip_insert(name, extension):
-    if len(os.listdir(os.getcwd() )) == 0: # проверка на пустой каталог
-        print("Directory is empty")
-    else:
-        print("Directory is not empty")
-    with ZipFile(name, 'w') as testzip: # создание рабочего архива
+    if len(os.listdir(os.getcwd())) == 0: #check whether the directory is empty
+        return None
+    with ZipFile(name, 'w') as testzip: # creating zip file
         for root, dirs, files in os.walk(os.getcwd()):
             for file in files:
-                if file.endswith(extension): #Внесение в архив файлов по признаку расширения
+                if file.endswith(extension): #importing file into zip file according to extension
                     testzip.write(os.path.join(root, file))
     return testzip.filename
+
 
 name = 'test.zip'
 extension = '.odg'
 print(zip_insert(name, extension))
+
